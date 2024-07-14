812fd76dc97fa5c5b5934f19421cab577e126483
Lara Emily Jacobs <23260122@sun.ac.za>
2024-07-14 17:11:18 +0200

    day 1 tasks

---
+++ day1/d1ex1.py
@@ -0,0 +1,24 @@
+#exercise 1 day 1: performing operations on 3 and 4.
+
+print("3+4=")
+print(3+4)
+
+print("3-4=")
+print(3-4)
+
+print("3*4=")
+print(3*4)
+
+print("3%4=")
+print(3%4)
+
+print("3/4=")
+print(3/4)
+
+print("3**4=")
+print(3**4)
+
+print("3//4=")
+print(3//4)
+
+
